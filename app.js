a1c425b193ed625555c92c468979354f8a995473
sajna <sajnaaizanaufal@gmail.com>
2023-07-15 14:49:20 +0530

    change

--- app.js
+++ app.js
@@ -1,71 +1,28 @@
-var nodemailer =require('nodemailer');
-// const express = require("express");
-// const app = express();
-let mailTransporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:"magicalfoodie123@gmail.com",
-        pass:"zqopyrgplmuzllwn"
-    }
+
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user:"magicalfoodie123@gmail.com",
+            pass:"zqopyrgplmuzllwn" 
+  }
 });
-let details={
+
+
+const mailOptions = {
     from:"magicalfoodie123@gmail.com",
-    to:"sajnaaizanaufal@gmail.com",
-    subject:"Hello,This is a test mail",
-    text:"This is the body of the mail"
-}
-mailTransporter.sendMail(details,(err)=>{
-    if (err) {
-        console.log("it has an error",err);
-    }
-    else{
-        console.log("email has send");
-    }
-})
+        to:"sajnaaizanaufal@gmail.com",
+        subject:"Hello,This is a test mail",
+        text:"This is the body of the mail"
+    };
 
-
-
-
-
-var nodemailer = require('nodemailer');
-const express = require("express");
-const app= new express();
-const PORT =5000;
-var transporter = nodemailer.createTransport({ 
-    service: 'gmail',
-
-      auth: {
-
-        user: 'magicalfoodie123@gmail.com', 
-        pass: 'saju@naufalm'
-
-}
-
-});
-
-var mailOptions = {
-
-      from: 'magicalfoodie123@gmail.com', 
-      to: 'sajnaaizanaufal@gmail.com', 
-      subject: 'Sending Email using Node.js', 
-      text: 'That was dsfgdfg not easy!'
-};
-
-transporter.sendMail(mailOptions, function (error, info){
-
-if (error) {
-
-    console.log(error);
-
-} else {
-
-console.log('Email sent:' + info.response);
-
-}
-
-});
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-});
-
-
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error occurred:', error);
+      } else {
+        console.log('Email sent successfully using gmail!');
+      }
+    });
